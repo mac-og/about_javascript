576eb7f21ce42ccbf2a2592be1e3760d1ef8454a
mac-og <macogleks@gmail.com>
2024-03-12 00:42:11 +0100

    about javascript DOM

---
+++ all_DOM/about_alert.js
@@ -0,0 +1,10 @@
+// An alert is a keyword and it pops up when a browser is opened to give a user a message
+window.alert("Welcome to this page !!!");
+
+/* The default usage is using 'windom.alert but alert can be also used 
+   and it does the same thing
+   */
+alert("Your input was accepted.");
+
+var textInput = "You are good to go";
+alert(textInput);
---
+++ all_DOM/about_prompt.js
@@ -0,0 +1,8 @@
+/*  A prompt provides a response field for a user to input data.
+    It also makes use of a default value in case no response is given
+*/
+
+// if no value is given then 'undefined' is atored
+let lang = window.prompt('What language do you prefer ?');
+
+const prefered = prompt('Preferd langauge ?', 'javaScrpt');
