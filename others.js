0dd405da69bde8d738521b08a3ac3f86c08f88e4
mac-og <macogleks@gmail.com>
2024-04-01 00:17:59 +0100

    update

--- others.js
+++
@@ -1,9 +0,0 @@
-let process = [
-    {"type": "stock fish",
-     "ingredints": [
-        {"name": "fish", "size": "1 kg"},
-        {"name": "stock", "size": "2 kg"},
-        {"name": "water", "size": "1 kg"}
-     ]},
-    {}
-]
